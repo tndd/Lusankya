3dd8cebad721753d768e107b0d45f52c0d1fc6b2
tau <36781951+tndd@users.noreply.github.com>
2023-12-07 12:37:07 +0900

    tmp impl new_async

--- src/infra/psql/client.rs
+++ src/infra/psql/client.rs
@@ -1,40 +1,23 @@
+use std::sync::{Arc, Mutex};
 use tokio_postgres::{Client, NoTls, Error};
 
 pub struct PsqlClient {
-    host: String,
-    user: String,
-    password: String,
-    db_name: String
+    client: Arc<Mutex<Client>>,
 }
 
 impl PsqlClient {
-    pub fn new(host: String, user: String, password: String, db_name: String) -> Self {
-        Self {
-            host,
-            user,
-            password,
-            db_name
-        }
-    }
-
-    pub async fn connect(&self) -> Result<Client, Error> {
-        let conn_string = format!(
-            "host={} user={} password={} dbname={}",
-            self.host,
-            self.user,
-            self.password,
-            self.db_name
-        );
+    pub async fn new_async(host: String, user: String, password: String, db_name: String) -> Result<Self, Error> {
+        let conn_string = format!("host={} user={} password={} dbname={}", host, user, password, db_name);
         let (client, connection) = tokio_postgres::connect(&conn_string, NoTls).await?;
 
-        // The connection object performs the actual communication with the database,
-        // so spawn it off to run on its own.
         tokio::spawn(async move {
             if let Err(e) = connection.await {
                 eprintln!("connection error: {}", e);
             }
         });
 
-        Ok(client)
+        Ok(Self {
+            client: Arc::new(Mutex::new(client)),
+        })
     }
 }
