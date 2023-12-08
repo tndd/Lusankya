6c7d08f18dfd68f1bade251d1e43cfbf95a5f4b7
tau <36781951+tndd@users.noreply.github.com>
2023-12-08 23:58:10 +0900

    add migration asset

---
+++ migrations/2023-12-08-145537_create_assets/up.sql
@@ -0,0 +1,16 @@
+-- Your SQL goes here
+
+CREATE TABLE asset (
+    id UUID NOT NULL,
+    version TIMESTAMP NOT NULL,
+    class TEXT NOT NULL,
+    exchange TEXT NOT NULL,
+    symbol TEXT NOT NULL,
+    status TEXT NOT NULL,
+    tradable BOOLEAN NOT NULL,
+    marginable BOOLEAN NOT NULL,
+    shortable BOOLEAN NOT NULL,
+    easy_to_borrow BOOLEAN NOT NULL,
+    fractionable BOOLEAN NOT NULL,
+    PRIMARY KEY (id, version)
+);
